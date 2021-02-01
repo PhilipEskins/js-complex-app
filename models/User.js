36747bf70d79e880795ea39f8080c67256d06158
Firebeard <philipeskins@gmail.com>
2021-01-31 18:18:55 -0800

    first commit

---
+++ models/User.js
@@ -0,0 +1,66 @@
+const validator = require("validator")
+const usersCollection = require('../db').collection("users")
+
+let User = function(data) {
+    this.data = data
+    this.errors = []
+}
+
+User.prototype.validate = function() {
+    if (this.data.username == "") {
+        this.errors.push("Provide a username")
+    }
+    if (this.data.username != "" && !validator.isAlphanumeric(this.data.username)) {
+        this.errors.push("Enter valid username")
+    }
+    if (!validator.isEmail(this.data.email)) {
+        this.errors.push("Provide an email")
+    }
+    if (this.data.password == "") {
+        this.errors.push("Provide a password")
+    }
+    if (this.data.password.length > 0 && this.data.password.length < 12) {
+        this.errors.push("Password not long enough")
+    }
+    if (this.data.password.length > 100) {
+        this.errors.push("Password too long")
+    }
+    if (this.data.username.length > 0 && this.data.username.length < 3) {
+        this.errors.push("Username not long enough")
+    }
+    if (this.data.username.length > 30) {
+        this.errors.push("Username too long")
+    }
+}
+
+User.prototype.cleanUp = function() {
+    if (typeof(this.data.username) != "string") {
+        this.data.username = ""
+    }
+    if (typeof(this.data.email) != "string") {
+        this.data.email = ""
+    }
+    if (typeof(this.data.password) != "string") {
+        this.data.password = ""
+    }
+
+    // get rid of bogus stuff
+    this.data = {
+        username: this.data.username.trim().toLowerCase(),
+        email: this.data.email.trim().toLowerCase(),
+        password: this.data.password
+    }
+}
+
+User.prototype.register = function() {
+    // Step 1 Validate user data
+    this.cleanUp()
+    this.validate()
+
+    // Step 2 Save user data into database
+    if (!this.errors.length) {
+        usersCollection.insertOne(this.data)
+    }
+}
+
+module.exports = User
