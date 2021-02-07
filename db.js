3a64e559f95d4261a9f92a354b4b71a7be7ad03c
Firebeard <philipeskins@gmail.com>
2021-02-06 18:14:01 -0800

    sessions

--- db.js
+++ db.js
@@ -2,9 +2,8 @@ const mongodb = require ('mongodb')
 const dotenv = require ('dotenv')
 dotenv.config()
 
-
 mongodb.connect(process.env.CONNECTIONSTRING, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
-    module.exports = client.db()
+    module.exports = client
     const app = require('./app')
     app.listen(process.env.PORT)
 })
