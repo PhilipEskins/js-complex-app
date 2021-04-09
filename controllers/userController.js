7ea9e2f96ea4419e7c2636ad4e485de09ec0d939
Firebeard <philipeskins@gmail.com>
2021-04-09 05:53:58 -0700

    start api

--- controllers/userController.js
+++ controllers/userController.js
@@ -64,6 +64,15 @@ exports.login = function(req, res) {
     })
 }
 
+exports.apiLogin = function(req, res) {
+    let user = new User(req.body)
+    user.login().then(function(result) {
+        res.json("Successful login")
+    }).catch(function(e) {
+        res.json("Incorrect login")
+    })
+}
+
 exports.logout = function(req, res) {
     req.session.destroy(function() {
         res.redirect('/')
